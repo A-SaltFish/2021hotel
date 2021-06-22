f6ee523dec587129aee2461faef6bd73d2d9707e
UncleBigLu <1807814821@qq.com>
2021-06-22 18:06:45 +0800

    Added json server, get city data from json server

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="search-root">
+    <div class="topic">
+      <h2>酒店预订：</h2>
+    </div>
+    <div class="select-root">
+      <el-select v-model="city" placeholder="请选择城市">
+        <el-option v-for="item in citys"
+                   :key="item.id"
+                   :label="item.name"
+                   :value="item.id"></el-option>
+      </el-select>
+    </div>
+    <div class="select-root">
+      <el-select v-model="rank" placeholder="请选择星级（可选）">
+        <el-option v-for="item in ranks"
+                   :key="item"
+                   :label="item"
+                   :value="item"></el-option>
+      </el-select>
+    </div>
+    <div class="button">
+      <el-button type="primary" @click="Onclick" >查询</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props:{
+    citys: Array,
+    ranks: Array
+  },
+  data() {
+    return {
+      city: '',
+      rank: ''
+    }
+  },
+  methods:{
+    Onclick(){
+      console.log(this.city, this.rank)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-root {
+  margin: 10px;
+  padding: 10px;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.topic {
+  color: white;
+  align-self: flex-start;
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,18 +2,37 @@
 <div>
   <MyHeadBar/>
   <ImgCarousel/>
+  <SearchBar :citys="citys" :ranks="ranks"/>
 </div>
 </template>
 
 <script>
 import MyHeadBar from "../components/MyHeadBar";
 import ImgCarousel from "../components/ImgCarousel";
+import SearchBar from "../components/SearchBar";
 
 export default {
   name: "Index",
   components: {
     MyHeadBar,
-    ImgCarousel
+    ImgCarousel,
+    SearchBar
+  },
+  data() {
+    return {
+      citys: [],
+      ranks: [1, 2, 3, 4, 5]
+    }
+  },
+  methods:{
+    async fetchCity() {
+      const res = fetch('/api/citys')
+      const data = (await res).json()
+      return data
+    }
+  },
+  async created() {
+    this.citys = await this.fetchCity()
   }
 }
 </script>
