ff66bf27e1adcbd4bf0f1ce16850e6ba0016a767
UncleBigLu <1807814821@qq.com>
2021-06-25 15:39:17 +0800

    Added baidu map to index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,6 +4,8 @@
   <ImgCarousel/>
   <SearchBar @search-hotels="fetchHotels" />
   <HotelList :hotels="hotels"/>
+  <baidu-map class="bm-view" ak="a4bZsXqS2yx7z1VlXaNjI60lbV9DHbYP"
+             :center="mapCenter" :zoom="mapZoom" @ready="mapHandler"></baidu-map>
 </div>
 </template>
 
@@ -13,6 +15,8 @@ import ImgCarousel from "../components/ImgCarousel";
 import SearchBar from "../components/SearchBar";
 import HotelList from "../components/HotelList";
 import axios from "axios";
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+
 
 export default {
   name: "Index",
@@ -20,17 +24,26 @@ export default {
     MyHeadBar,
     ImgCarousel,
     SearchBar,
-    HotelList
+    HotelList,
+    BaiduMap
   },
   data() {
     return {
-      hotels:[]
+      hotels:[],
+      mapCenter: {lng: 0, lat: 0},
+      mapZoom: 3
     }
   },
   methods:{
     async fetchHotels(city){
       const res = await axios.get('/api/hotels?' + new URLSearchParams({ht_city: city}))
       this.hotels = res.data
+    },
+    mapHandler({BMap, map}) {
+      console.log(BMap, map)
+      this.center.lng = 116.404
+      this.center.lat = 39.915
+      this.zoom = 15
     }
   }
 };
@@ -39,5 +52,9 @@ export default {
 <style scoped>
 .element-root {
  overflow: auto;
+}
+.bm-view {
+  width: 100%;
+  height: 300px;
 }
 </style>
