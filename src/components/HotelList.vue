e99666f64b6e8fb11d613a8ebd320554a228e0f1
UncleBigLu <1807814821@qq.com>
2021-06-25 16:10:35 +0800

    moved map to HotelList

--- src/components/HotelList.vue
+++ src/components/HotelList.vue
@@ -1,22 +1,52 @@
 <template>
-  <div>
-    <Hotel v-for="hotel in hotels" :key="hotel.hotel_name" :hotel="hotel"/>
-  </div>
+  <el-container>
+    <el-main>
+      <Hotel v-for="hotel in hotels" :key="hotel.hotel_name" :hotel="hotel"/>
+    </el-main>
+    <el-aside width="400px">
+      <baidu-map class="bm-view" ak="a4bZsXqS2yx7z1VlXaNjI60lbV9DHbYP"
+                 :center="mapCenter" :zoom="mapZoom" @ready="mapHandler"></baidu-map>
+    </el-aside>
+  </el-container>
+
+
 </template>
 
 <script>
 import Hotel from "./Hotel";
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
 export default {
   name: "HotelList",
   components:{
-    Hotel
+    Hotel,
+    BaiduMap
   },
   props:{
     hotels:Array
+  },
+  data() {
+    return {
+      mapCenter: {lng: 0, lat: 0},
+      mapZoom: 3
+    }
+  },
+  methods: {
+    mapHandler({BMap, map}) {
+      console.log(BMap, map)
+      this.mapCenter.lng = 116.404
+      this.mapCenter.lat = 39.915
+      this.mapZoom = 16
+    }
   }
 }
 </script>
 
 <style scoped>
-
+el-container {
+  height: 100vh;
+}
+.bm-view {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,8 +4,6 @@
   <ImgCarousel/>
   <SearchBar @search-hotels="fetchHotels" />
   <HotelList :hotels="hotels"/>
-  <baidu-map class="bm-view" ak="a4bZsXqS2yx7z1VlXaNjI60lbV9DHbYP"
-             :center="mapCenter" :zoom="mapZoom" @ready="mapHandler"></baidu-map>
 </div>
 </template>
 
@@ -15,7 +13,6 @@ import ImgCarousel from "../components/ImgCarousel";
 import SearchBar from "../components/SearchBar";
 import HotelList from "../components/HotelList";
 import axios from "axios";
-import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
 
 
 export default {
@@ -24,26 +21,17 @@ export default {
     MyHeadBar,
     ImgCarousel,
     SearchBar,
-    HotelList,
-    BaiduMap
+    HotelList
   },
   data() {
     return {
-      hotels:[],
-      mapCenter: {lng: 0, lat: 0},
-      mapZoom: 3
+      hotels:[]
     }
   },
   methods:{
     async fetchHotels(city){
       const res = await axios.get('/api/hotels?' + new URLSearchParams({ht_city: city}))
       this.hotels = res.data
-    },
-    mapHandler({BMap, map}) {
-      console.log(BMap, map)
-      this.center.lng = 116.404
-      this.center.lat = 39.915
-      this.zoom = 15
     }
   }
 };
@@ -53,8 +41,5 @@ export default {
 .element-root {
  overflow: auto;
 }
-.bm-view {
-  width: 100%;
-  height: 300px;
-}
+
 </style>
